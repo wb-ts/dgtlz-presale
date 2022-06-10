5770534e9d312ac627d46054aa1a1527540c8513
SenDev001 <sergeilipovsky@gmail.com>
2022-06-10 09:19:54 +0100

    initial commit

---
+++ assets/js/client.js
@@ -0,0 +1,39 @@
+// $(document).ready(function(){
+
+// if($('.brands_slider').length)
+// {
+// var brandsSlider = $('.brands_slider');
+
+// brandsSlider.owlCarousel(
+// {
+// loop:true,
+// autoplay:true,
+// autoplayTimeout:5000,
+// nav:false,
+// dots:false,
+// autoWidth:true,
+// items:8,
+// margin:42
+// });
+
+// if($('.brands_prev').length)
+// {
+// var prev = $('.brands_prev');
+// prev.on('click', function()
+// {
+// brandsSlider.trigger('prev.owl.carousel');
+// });
+// }
+
+// if($('.brands_next').length)
+// {
+// var next = $('.brands_next');
+// next.on('click', function()
+// {
+// brandsSlider.trigger('next.owl.carousel');
+// });
+// }
+// }
+
+
+// });
